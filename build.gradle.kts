0aadb599bfd25c1f21058a9bed9941892a6257c5
Seavpheng PEUNG <seavpheng.peung@chipmongbank.com>
2026-07-01 15:41:51 +0700

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+	kotlin("jvm") version "2.3.21"
+	kotlin("plugin.spring") version "2.3.21"
+	id("org.springframework.boot") version "4.1.0"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("com.google.protobuf") version "0.9.6"
+}
+
+group = "com.chipmong"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-grpc-client")
+	implementation("org.springframework.boot:spring-boot-starter-grpc-server")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("tools.jackson.module:jackson-module-kotlin")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-grpc-client-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-grpc-server-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testCompileOnly("org.projectlombok:lombok")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testAnnotationProcessor("org.projectlombok:lombok")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/chipmong/gRPC_SubModule_Testing/GRpcSubModuleTestingApplication.kt
@@ -0,0 +1,11 @@
+package com.chipmong.gRPC_SubModule_Testing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GRpcSubModuleTestingApplication
+
+fun main(args: Array<String>) {
+	runApplication<GRpcSubModuleTestingApplication>(*args)
+}
