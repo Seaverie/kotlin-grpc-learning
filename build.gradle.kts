3cfb5e066a40675bdd4af566816553dc48177e81
Seavpheng PEUNG <seavpheng.peung@chipmongbank.com>
2026-07-01 17:04:48 +0700

    create customer-service and modified the root build.gradle.kts so that every sub-module don't have to do anything in build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,63 @@
 plugins {
-	kotlin("jvm") version "2.3.21"
-	kotlin("plugin.spring") version "2.3.21"
-	id("org.springframework.boot") version "4.1.0"
-	id("io.spring.dependency-management") version "1.1.7"
-	id("com.google.protobuf") version "0.9.6"
+	kotlin("plugin.spring") version "2.3.21" apply false
+	id("org.springframework.boot") version "4.1.0" apply false
+	id("io.spring.dependency-management") version "1.1.7" apply false
+	id("com.google.protobuf") version "0.9.6" apply false
+	kotlin("jvm") version "2.4.0"
 }
 
 group = "com.chipmong"
 version = "0.0.1-SNAPSHOT"
 
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
+allprojects {
+	group = "com.chipmong"
+	version = "0.0.1-SNAPSHOT"
+
+	repositories {
+		mavenCentral()
 	}
 }
 
+subprojects {
+	apply(plugin = "org.jetbrains.kotlin.jvm")
+	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
+	apply(plugin = "com.google.protobuf")
+
+	// Use configure<T> to target the extensions explicitly
+	configure<org.gradle.api.plugins.JavaPluginExtension> {
+		toolchain {
+			languageVersion.set(JavaLanguageVersion.of(17))
+		}
+	}
+
+	configure<org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension> {
+		jvmToolchain(17)
+	}
+
+	// Use string literals "implementation" and "testImplementation"
+	dependencies {
+		"implementation"("org.jetbrains.kotlin:kotlin-reflect")
+		"implementation"("tools.jackson.module:jackson-module-kotlin")
+
+		"implementation"("io.grpc:grpc-stub")
+		"implementation"("io.grpc:grpc-protobuf")
+
+		"implementation"("org.springframework.boot:spring-boot-starter-grpc-server")
+		"implementation"("org.springframework.boot:spring-boot-starter-grpc-client")
+
+		// Replaced kotlin("test") shorthand with its explicit coordinate string
+		"testImplementation"("org.jetbrains.kotlin:kotlin-test")
+	}
+
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
+}
+dependencies {
+	implementation(kotlin("stdlib-jdk8"))
+}
 repositories {
 	mavenCentral()
-}
-
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-grpc-client")
-	implementation("org.springframework.boot:spring-boot-starter-grpc-server")
-	implementation("org.springframework.boot:spring-boot-starter-webmvc")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("tools.jackson.module:jackson-module-kotlin")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-grpc-client-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-grpc-server-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testCompileOnly("org.projectlombok:lombok")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	testAnnotationProcessor("org.projectlombok:lombok")
-}
-
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
-	}
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "gRPC-SubModule-Testing"
+
+include("services:customer-service")
